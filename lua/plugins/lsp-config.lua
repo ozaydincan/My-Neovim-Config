bd83f76fb4ed6f423ec182ce6ab034bc382d8b4d
Can Ozaydin <ozaydincan.app@gmail.com>
2024-11-20 16:46:11 +0100

    Version conflicts removed

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -7,32 +7,28 @@ return {
     end
   },
   {
-  'williamboman/mason-lspconfig.nvim',
-  lazy = false,
-  opts = {
-    auto_install = true,
+    'williamboman/mason-lspconfig.nvim',
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "pyright",
+          "clangd",
+          "arduino_language_server",
+          "cmake",
+          "rust_analyzer",
+          "sqlls",
+          "zls",
+        }
+
+      })
+    end
+
   },
-  config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "lua_ls",
-        "gopls",
-        "pyright",
-        "clangd",
-        "arduino_language_server",
-        "cmake",
-        "rust_analyzer",
-        "docker_compose_language_service",
-        "sqlls",
-        "zls",
-        "vimls",
-        "svelte",
-      }
-
-    })
-  end
-
-},
   {
     "neovim/nvim-lspconfig",
     config = function()
